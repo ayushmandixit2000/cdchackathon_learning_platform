bc558b0a9fb4a0b1fbe8480d71b1a84fc3ec5ea4
ayushmandixit2000 <ayushmandixit2000@gmail.com>
2022-10-09 02:13:22 +0800

    added com

--- src/components/DevCom/DevCom.js
+++ src/components/DevCom/DevCom.js
@@ -6,7 +6,9 @@ import notFound from "../../images/undraw_Taken_re_yn20.svg";
 const DevCom = () => {
    return (
       <div className="dev-com">
+         
          <div className="container text-center">
+         <h4> Get help from the community & Answer to earn</h4>
             <iframe src="https://cronos.org/community" width="100%" height="1000">
   <p>Your browser does not support iframes.</p>
 </iframe>
